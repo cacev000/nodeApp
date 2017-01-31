be32cc3a8850cfe10542d85e3bef2e9361a887a0
Carlos Acevedo <cacevedo@brandinstitute.com>
2017-01-31 17:25:38 -0500

    first commit

---
+++ server/routes/api.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+
+// declare axios for making http requests
+const axios = require('axios');
+const API = 'https://jsonplaceholder.typicode.com';
+
+/* GET api listing. */
+router.get('/', (req, res) => {
+    res.send('api works');
+});
+
+var AWS = require('aws-sdk');
+
+AWS.config.region = "us-east-1";
+
+var sns = new AWS.SNS();
+var ddb = new AWS.DynamoDB();
+
+var params = {
+    TableName: "Client"
+};
+
+// Get all posts
+router.get('/posts', (req, res) => {
+    // Get posts from the mock api
+    // This should ideally be replaced with a service that connects to MongoDB
+    // axios.get(`${API}/posts`)
+    //     .then(posts => {
+    //         res.status(200).json(posts.data);
+    //     })
+    //     .catch(error => {
+    //         res.status(500).send(error)
+    //     });
+
+    ddb.scan(params, onScan);
+
+    function onScan(err, data) {
+        if (err) {
+            console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            // print all the movies
+            console.log("Scan succeeded.");
+            console.log(data)
+        }
+    }
+});
+
+module.exports = router;
